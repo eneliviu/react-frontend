4be782ddef60988613a13f0dbac504baf8cf8974
Liviu Ene <eneliviu@gmail.com>
2024-12-22 19:41:56 +0100

    Create components

--- fronted-api/src/index.js
+++ fronted-api/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
+import {
+  Route, BrowserRouter as Router, Switch
+} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Login from './Login';
+import Register from './Register';
+import Header from './Header';
+import Footer from './Footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const routing = (
+    <Router>
+        <React.StrictMode>
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+                <Footer />
+            </div>
+        </React.StrictMode>
+    </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
